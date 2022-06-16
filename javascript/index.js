35df258cca9b0cb261d87fed52d5158c9b5326ac
udayan-majumder <udayanmajumder1983@gmail.com>
2022-06-17 01:14:18 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,51 @@
+var main_var= document.getElementById('field');
+function myfunction(){
+    let newvalue=eval(main_var.value);
+    main_var.value = newvalue;
+}
+
+function firstfunction(){
+    main_var.value+='1'
+}
+function secondfunction(){
+    main_var.value+='2'
+}
+function thirdfunction(){
+    main_var.value+='3'
+}
+function plusfunction(){
+    main_var.value+='+'
+}
+function fourthfunction(){
+    main_var.value+='4'
+}
+function fifthfunction(){
+    main_var.value+='5'
+}
+function sixthfunction(){
+    main_var.value+='6'
+}
+function minusfunction(){
+    main_var.value+='-'
+}
+function seventhfunction(){
+    main_var.value+='7'
+}
+function eigthfunction(){
+    main_var.value+='8'
+}
+function ninthfunction(){
+    main_var.value+='9'
+}
+function dividefunction(){
+    main_var.value+='/'
+}
+function zerofunction(){
+    main_var.value+='0'
+}
+function clearfunction(){
+    main_var.value=''
+}
+function multiplyfunction(){
+    main_var.value+='x'
+}
